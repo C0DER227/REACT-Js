8a11fe870e32d980187ba91f92c208bc3b46419e
Coder22794 <inbuiltcoder22794@gmail.com>
2022-11-13 01:24:22 +0530

    my-todo

---
+++ my-todo/src/App.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+import './App.css'
+
+function App() {
+  const [todolist,Settodolist]=useState([])
+  const [newtask,Setnewtask]=useState("")
+
+  const handleChange=(e)=>{
+    Setnewtask(e.target.value)
+  }
+
+  const addtask=()=>{
+    Settodolist([...todolist, newtask])
+  }
+
+  const deletetask=(taskName)=>{
+    const newtodolist=todolist.filter((task)=>task!==taskName);
+    Settodolist(newtodolist)
+  }
+  return (
+    <div>
+      <h2>TODO-LIST</h2>
+      <div className='task'>
+        <input onChange={handleChange}/>
+        <button onClick={addtask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todolist.map((task)=>{
+          return <div>
+            <h1>{task}</h1>
+          <button className='btn' onClick={()=>deletetask(task)}>X</button></div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
